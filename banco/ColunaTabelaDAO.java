ac52368311f436653c06a25c7fe5c7ea7289169b
MagdielPr <152613195+MagdielPr@users.noreply.github.com>
2024-06-22 17:23:14 -0300

    Update ColunaTabelaDAO.java

--- banco/ColunaTabelaDAO.java
+++ banco/ColunaTabelaDAO.java
@@ -1,13 +1,13 @@
 package banco;
 
+import classes.Coluna;
+import classes.Tipo;
 import java.sql.SQLException;
 import java.util.List;
-import classes.Coluna;
-//iNTERFACE PARA GERENCIAR AS COLUNAS DE UMA TABELA DO BANCO, TA SAFE '-'
 
-public interface ColunaTabelaDAO {
-    public void adicionarColuna(String nomeTabela, Coluna coluna) throws SQLException;
-    public void alterarColuna(String nomeTabela, Coluna coluna) throws SQLException;
-    public void removerColuna(String nomeTabela, String nomeColuna) throws SQLException;
-    List<Coluna> listarColunas(String nomeTabela) throws SQLException;
+public interface ColunaTabelaDAO<T extends Coluna<? extends Tipo>> {
+    void adicionarColuna(String nomeTabela, T coluna) throws SQLException;
+    void alterarColuna(String nomeTabela, T coluna) throws SQLException;
+    void removerColuna(String nomeTabela, String nomeColuna) throws SQLException;
+    List<T> listarColunas(String nomeTabela) throws SQLException;
 }
