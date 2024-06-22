340bf269992f8e93144d9e6b9464102b67e110c2
MagdielPr <152613195+MagdielPr@users.noreply.github.com>
2024-06-22 17:23:34 -0300

    Update BancoDadosDAO.java

--- banco/BancoDadosDAO.java
+++ banco/BancoDadosDAO.java
@@ -1,11 +1,11 @@
 package banco;
 
+import classes.BancoDados;
 import java.sql.SQLException;
 import java.util.List;
-import classes.BancoDados;
-// iNTERFACE PARA GERENCIAR O BANCO, TA FUNCIONANDO POR ENQUANTO '-'
-public interface BancoDadosDAO {
-	public void criarBancoDados(BancoDados bancoDados) throws SQLException;
-	public void removerBancoDados(String nomeBanco) throws SQLException;
-    List<BancoDados> listarBancosDados() throws SQLException;
+
+public interface BancoDadosDAO<T extends BancoDados> {
+    void criarBancoDados(T bancoDados) throws SQLException;
+    void removerBancoDados(String nomeBanco) throws SQLException;
+    List<T> listarBancosDados() throws SQLException;
 }
