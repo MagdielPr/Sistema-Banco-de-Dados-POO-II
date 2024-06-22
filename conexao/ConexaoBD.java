ff01c745aeef28f7a79dc1b7dad7326c4404a269
MagdielPr <152613195+MagdielPr@users.noreply.github.com>
2024-06-22 17:17:32 -0300

    Update ConexaoBD.java

--- conexao/ConexaoBD.java
+++ conexao/ConexaoBD.java
@@ -4,31 +4,28 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConexaoBD {
+public class ConexaoBD<T extends ConfiBD> {
+    private final T config;
 
-	// criarbanco(nome banco)
-    // Dados da conexão do workbench senha 1234, lembrar de implementar para o usuário passar a conexão/usuario/senha/porta
-    private static final String URL = "jdbc:mysql://localhost:3306/sqlconnection";
-    private static final String BANCO = "";
-    private static final String USUARIO = "root";
-    private static final String SENHA = "1234";
-    private static final String PORTA = "";
-    
-    // Método para pegar a conexão
-    public static Connection getConnection() throws SQLException {
-        Connection connection = null;
+    public ConexaoBD(T config) {
+        this.config = config;
+    }
+
+    public Connection getConnection() throws SQLException {
+        String url = String.format("jdbc:mysql://%s:%d/%s", 
+            config.getHost(), config.getPort(), config.getDatabase().getName());
+        
         try {
-            connection = DriverManager.getConnection(URL, USUARIO, SENHA);
-            System.out.println("Conectado ao banco de dados com sucesso!");
+            Connection connection = DriverManager.getConnection(url, config.getUsername(), config.getPassword());
+            System.out.println("Conectado ao banco de dados " + config.getDatabase().getName() + " com sucesso!");
+            return connection;
         } catch (SQLException e) {
             System.err.println("Erro ao conectar ao banco de dados: " + e.getMessage());
             throw e;
         }
-        return connection;
     }
 
-    // Fechar a conexão
-    public static void closeConnection(Connection connection) {
+    public void closeConnection(Connection connection) {
         if (connection != null) {
             try {
                 connection.close();
@@ -36,18 +33,6 @@ public class ConexaoBD {
             } catch (SQLException e) {
                 System.err.println("Erro ao fechar a conexão: " + e.getMessage());
             }
-        }
-    }
-
-    // Teste de conexão '-'
-    public static void main(String[] args) {
-        Connection connection = null;
-        try {
-            connection = ConexaoBD.getConnection();
-        } catch (SQLException e) {
-            System.err.println("Erro durante o teste de conexão: " + e.getMessage());
-        } finally {
-            closeConnection(connection);
         }
     }
 }
